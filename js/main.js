82c17704b9116822cda48b43a0c4bfed7bf9879f
Martin <martinchauque09@gmail.com>
2022-08-27 01:33:33 -0300

    First Commit

--- js/main.js
+++ js/main.js
@@ -39,3 +39,28 @@ function redirectUrl(){
 
 let boton = document.getElementById("enviar");
 boton.addEventListener("click", onClick);
+
+let url = 'https://api.openweathermap.org/data/2.5/weather?lat=-24.183241513577308&lon=-65.3312335299548&appid=a2a967f20fda739b027a2e0aa283722b&lang=es&units=metric';
+
+
+//solicitud
+fetch(url)
+  .then(response => response.json())  //resuelve la promesa, entonces lo maneja con un json()
+  .then(data => mostrarData(data))  //aqui maneja los datos, llama la funcion pasando como parametro data
+  .catch(error => console.log(error))  //en caso de error dispara el catch
+
+
+const mostrarData = (data)=>{
+  console.log(data);
+  
+   let city = data['name'];
+   let temp = data['main']['temp'];
+   let time = data['weather'][0]['description'];
+
+   let body = '';
+  
+    body+= `<tr><td> ${city} </td><td> ${temp} </td><td> ${time} </td></tr>`
+  
+  // document.getElementById('temperatura').innerHTML = temp;
+  document.getElementById('data').innerHTML = body;
+}
